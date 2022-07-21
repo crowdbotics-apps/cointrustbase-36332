184283bd621f87883812b5735a87b35bd960dbe0
Crowdbotics <team@crowdbotics.com>
2022-07-21 14:29:44 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cointrustbase_36332/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cointrustbase_36332;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "cointrustbase_36332";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cointrustbase_36332",
+  url: "https://cointrustbase_36332.botics.co",
+  api: "https://cointrustbase_36332.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
